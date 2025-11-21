4b7abcb4edb681d56abcb71c218c218a28699dad
bhavanachoudhari24 <bhavana.choudhari24@gmail.com>
2025-11-21 17:00:53 +0530

    Delete 3 tier-project-scrach directory

--- 3 tier-project-scrach/terraform/user_data/nexus_user_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras install java-openjdk17 -y
-yum install -y docker
-service docker start
-usermod -a -G docker ec2-user
-# run Nexus OSS
-docker run -d --name nexus -p 8081:8081 -v /nexus-data:/nexus-data sonatype/nexus3
